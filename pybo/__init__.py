451d1414ab33d253658e9f41a7b46ecfe88c89f8
kimdabin <favorite0218@naver.com>
2026-07-30 12:17:32 +0900

    20260730 이미지 업로드(1개 완)

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -2,19 +2,42 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from pybo.filter import format_datetime
-
+import markdown
+from markupsafe import Markup
 import config
 
 db = SQLAlchemy()
 migrate = Migrate()
 
+# 애플리케이션 팩토리 함수 정의
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
 
     # ORM 초기화
+    from . import models
     db.init_app(app)
     migrate.init_app(app, db)
+
+    # 1. 마크다운 변환 함수를 선언합니다.
+    def format_markdown(text):
+        if not text:
+            return ""
+
+        # markdown.markdown()의 결과(문자열)를 MarkupSafe의 Markup 객체로 감싸줍니다
+        # 이렇게 감싸줘야 템플릿(HTML)에서 꺾쇠 태그가 무력화되지 않고 화면에 잘 나옵니다
+        html_content = markdown.markdown(text,
+                extensions=['nl2br', 'fenced_code', 'sane_lists', 'tables'])
+
+        # Bootstrap 클래스 추가
+        html_content = html_content.replace(
+            '<table>',
+            '<table class="table tale-bordered table-hover">'
+        )
+        return Markup(html_content)
+
+    # 2. Flask 앱의 Jinja2 템플릿 필터로 등록합니다. (필터 이름: 'markdown')
+    app.jinja_env.filters['markdown'] = format_markdown
 
     # jinja_env 필터에 등록
     app.jinja_env.filters['datetime'] = format_datetime
--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -19,7 +19,11 @@ def create(question_id):
        answer = Answer(content=content, create_date=datetime.now(), user=g.user)
        question.answer_set.append(answer)
        db.session.commit()
-       return redirect(url_for('question.detail', question_id=question_id))
+       # return redirect(url_for('question.detail', question_id=question_id))
+       # 답변 등록시 앵커 기능 추가
+       return redirect('{}#answer_{}'.format(
+           url_for('question.detail', question_id=question_id), answer.id
+       ))
     return render_template('question/question_detail.html', question=question, form=form)
 
 @bp.route('/modify/<int:answer_id>/', methods=('GET', 'POST'))
@@ -35,7 +39,11 @@ def modify(answer_id):
         if form.validate_on_submit():
             form.populate_obj(answer)
             db.session.commit()
-            return redirect(url_for('question.detail', question_id=answer.question.id))
+            # return redirect(url_for('question.detail', question_id=answer.question.id))
+            # 답변 수정시 앵커 기능 추가
+            return redirect('{}#answer_{}'.format(
+                url_for('question.detail', question_id=answer.question_id), answer.id
+            ))
     else:
          form = AnswerForm(obj=answer)
     return render_template('answer/answer_form.html', answer=answer, form=form)
@@ -50,4 +58,25 @@ def delete(answer_id):
     else:
         db.session.delete(answer)
         db.session.commit()
+    return redirect(url_for('question.detail', question_id=question_id))
+
+@bp.route('/vote/<int:answer_id>/')
+@login_required
+def vote(answer_id):
+    answer = Answer.query.get_or_404(answer_id)
+    question_id = answer.question.id
+    # 로그인한 사용자가 본인의 글을 추천하는것을 막음
+    if g.user == answer.user:
+       flash('본인이 작성한 글은 추천할 수 없습니다')
+       return redirect(url_for('question.detail', question_id=question_id))
+
+    # 중복 추천 방지 로직
+    if g.user in answer.voter:
+        flash('이미 추천한 질문입니다')
+        return redirect(url_for('question.detail', question_id=question_id))
+    
+    # 기존 추천 처리 로직
+    answer.voter.append(g.user)
+    db.session.commit()
+
     return redirect(url_for('question.detail', question_id=question_id))
